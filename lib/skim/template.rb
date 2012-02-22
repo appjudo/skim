bc54f8d010e73feef94ae586462ea4e664274227
Yury Korolev <yury.korolev@gmail.com>
2012-02-22 23:29:07 +0400

    Closes #2; Moved skim js helpers to separate file
    - Moved JS helpers to vendor/assets/javascripts/skim.js.coffee
    - Introduced :use_asset Skim::Engine option (default to false)

--- lib/skim/template.rb
+++ lib/skim/template.rb
@@ -3,47 +3,15 @@ module Skim
 
   class Template
     def coffee_script_src
+      engine = self.class.build_engine({
+        :streaming => false, # Overwrite option: No streaming support in Tilt
+        :file => eval_file,
+        :indent => 2 }, options)
       <<SRC
 (context = {}) ->
-  _access = (name) ->
-    value = @[name]
-    value = value.call(@) if typeof value == "function"
-    return [@]            if value == true
-    return false          if value == false or !value?
-    return [value]        if Object.prototype.toString.call(value) != "[object Array]"
-    return false          if value.length == 0
-    return value
-
-  _withContext = (context, block) ->
-    create = (o) ->
-      F = ->
-      F.prototype = o
-      new F
-
-    context = create(context)
-
-    context.safe ||= @safe || (value) ->
-      return value if value?.skimSafe
-      result = new String(value ? '')
-      result.skimSafe = true
-      result
-
-    context.escape ||= @escape || (string) ->
-      return '' unless string?
-      return string if string.skimSafe
-      (''+string)
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;')
-        .replace(/\\//g,'&#47;')
-
-    block.call(context)
-
-  _withContext.call {}, context, ->
-#{self.class.build_engine({ :streaming => false, # Overwrite option: No streaming support in Tilt
-                            :file => eval_file,
-                            :indent => 2 }, options).call(data)}
+  #{self.class.skim_src unless engine.options[:use_asset]}
+  Skim.withContext.call {}, context, ->
+#{engine.call(data)}
 SRC
     end
 
@@ -53,6 +21,11 @@ SRC
 
     def evaluate(scope, locals, &block)
       precompiled_template
+    end
+
+    def self.skim_src
+      @@skim_src ||=
+        File.read(File.expand_path("../../../vendor/assets/javascripts/skim.js.coffee", __FILE__))
     end
   end
 end
